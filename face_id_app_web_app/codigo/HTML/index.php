c201ab9e2c237ed5f7508a16eb54d29e1c9e66cb
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-05 22:09:23 -0600

    ok

---
+++ face_id_app_web_app/codigo/HTML/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link href="/face_id_app_web_app/codigo/css/index.css"  rel="stylesheet"  type="text/css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+    <div id="video-background" ><video src="/face_id_app_web_app/media/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 slide">
+                
+                hola
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
