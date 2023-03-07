1f7f091fb9a6c81f4e1e93ae6114d3c03ee16c72
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-06 23:31:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <link rel="stylesheet" href="/public/css/index.css" type="text/css">
+    <link rel="stylesheet" href="/public/css/index.css" type="text/css"> 
 
 
     
