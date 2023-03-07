39387b5d26b3557dcadbea2bc183d156860dbaa9
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-06 21:23:22 -0600

    cambio de direccion

--- index.php
+++ index.php
@@ -14,8 +14,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <link href="/face_id_app_web_app/codigo/css/index.css"  rel="stylesheet"  type="text/css">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" href="/public/css/index.css" type="text/css">
+
     
 </head>
 <body>
