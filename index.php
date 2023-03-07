ad3290c54c15792d13108f9f9fe98ce6c1828022
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-06 22:32:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 </head>
 <body > 
     <div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
-    <div class="container">
+    <div class="container mx-auto">
     <br><br><br>
         <div class="row">
             <h1>Bienvenido al sistema de control:</h1><br>
