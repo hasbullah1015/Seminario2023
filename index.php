2649b3eb3a7c2ed9c40bbb861711a40950937261
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-06 21:35:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
 
             <form method="post" action="index_panel.html">
             <label for="user">Nombre: </label>
+            <label for="password">Contraseña: </label>
                 <input type="text" name="user" id="user"hNombre: ><br>
                 <input type="password" name="password" id="password"><br>
                 <button type="submit">Send</button>
