8f11ea171b9377a969c6457dff21cae58cc197c0
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-06 22:36:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 </head>
 <body > 
     <div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
-    <div class="container ">
+    <div class="container">
     <br><br><br>
-        <div class="row mx-auto">
+        <div class="row ">
             <h1>Bienvenido al sistema de control:</h1><br>
             <h2>Por favor, ingrese su usuario y contraseña: </h2>
         </div>   
