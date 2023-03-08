0198336221da05bf34ec58446a862a16f67eae7c
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-07 21:00:00 -0600

    Update databaseconnection.php

--- public/php/databaseconnection.php
+++ public/php/databaseconnection.php
@@ -1,7 +1,7 @@
 <?php
     $dbhost = "127.0.0.1";
     $dbuser = "u614989997_admindb";
-    $dbpswd = "6TUH72toIDh5RpVGBi";
+    $dbpswd = "6TUH72toIDh5RpVGBif";
     $dbname = "u614989997_db_inventario";
 
     $connection= mysqli_connect($dbhost,$dbuser,$dbpswd,$dbname);
