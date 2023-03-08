a427c21531f638c6caad785f6235611b591b4221
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-07 21:46:45 -0600

    Update databaseconnection.php

--- public/php/databaseconnection.php
+++ public/php/databaseconnection.php
@@ -17,7 +17,7 @@
     $pswd = $_POST['password'];
     $sqlquery =mysqli_query($connection,"SELECT * FROM  administracion WHERE usr = '$user' AND pass ='$pswd'");
     if($sqlquery->num_rows > 0){
-        header("public/html/index_panel.html");
+        include("public/html/index_panel.html");
     }
     else{
         echo "usuario no encontrado";
