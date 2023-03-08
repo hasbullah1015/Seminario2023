15753e41e0d2090b5d90d4063abdf4f9b6582be0
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-07 22:48:51 -0600

    f

--- index.php
+++ index.php
@@ -22,8 +22,7 @@
         <div class="row text-center">
         <b><h1 class="display-1">Bienvenido,</h1></b><br>
         <h2 class="display-2">Por favor, ingrese su usuario y contraseña:</h2><br>
-
-        </div>   
+</div>   
 
         <div class="row">
             <div class="col-sm-3 col-md-3 mx-auto slide">
