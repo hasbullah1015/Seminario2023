411959ee85ff8cd42587ae5461563f1840b0eaac
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-07 22:01:40 -0600

    Update databaseconnection.php

--- public/php/databaseconnection.php
+++ public/php/databaseconnection.php
@@ -17,7 +17,7 @@
     $pswd = $_POST['password'];
     $sqlquery =mysqli_query($connection,"SELECT * FROM  administracion WHERE usr = '$user' AND pass ='$pswd'");
     if($sqlquery->num_rows > 0){
-        header('Location: public/html/index_panel.html');
+        header('Location: https://seminario2023.website/public/html/index_panel.html');
 
     }
     else{
