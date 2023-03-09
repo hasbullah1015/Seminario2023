91c81c780314c92adc096871fc2cecc112192905
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-08 20:24:34 -0600

    Update login.php

--- public/php/login.php
+++ public/php/login.php
@@ -1,6 +1,17 @@
 <?php
-session_start();
-require_once 'databaseconnection.php';
+
+    $dbhost = "127.0.0.1";
+    $dbuser = "u614989997_admindb";
+    $dbpswd = "6TUH72toIDh5RpVGBi";
+    $dbname = "u614989997_db_inventario";
+
+    $connection= mysqli_connect($dbhost,$dbuser,$dbpswd,$dbname);
+    if(!$connection){
+
+        die("no se logro hacer la conexión con la db admin: ".mysqli_error());
+    }
+
+    mysqli_close($connection);
 
 
     $user = $_POST['user'];
