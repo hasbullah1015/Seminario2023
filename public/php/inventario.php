05769ecd14ef2b99184ee32121432caa071fc44c
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-08 22:12:37 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -1,18 +1,4 @@
-<?php
 
-    $dbhost = "127.0.0.1";
-    $dbuser = "u614989997_admindb";
-    $dbpswd = "6TUH72toIDh5RpVGBi";
-    $dbname = "u614989997_db_inventario";
-
-    $connection= mysqli_connect($dbhost,$dbuser,$dbpswd,$dbname);
-    if(!$connection){
-
-        die("no se logro hacer la conexión con la db admin: ".mysqli_error());
-    }
-    echo 
-
-?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -24,6 +10,16 @@
 </head>
 <body>
     <?php
+            $dbhost = "127.0.0.1";
+            $dbuser = "u614989997_admindb";
+            $dbpswd = "6TUH72toIDh5RpVGBi";
+            $dbname = "u614989997_db_inventario";
+        
+            $connection= mysqli_connect($dbhost,$dbuser,$dbpswd,$dbname);
+            if(!$connection){
+        
+                die("no se logro hacer la conexión con la db admin: ".mysqli_error());
+            }
          $sqlquery =mysqli_query($connection,"SELECT * FROM inventario WHERE idProducto='LAC-01'");
         $datos=mysqli_fetch_row($sqlquery);
         if(!$datos){echo 'error';}
