d5f6051f6b31d41771bc7422d9b9d0aa5254c8dd
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-08 22:05:21 -0600

    ok

---
+++ public/php/inventario.php
@@ -0,0 +1,43 @@
+<?php
+
+    $dbhost = "127.0.0.1";
+    $dbuser = "u614989997_admindb";
+    $dbpswd = "6TUH72toIDh5RpVGBi";
+    $dbname = "u614989997_db_inventario";
+
+    $connection= mysqli_connect($dbhost,$dbuser,$dbpswd,$dbname);
+    if(!$connection){
+
+        die("no se logro hacer la conexión con la db admin: ".mysqli_error());
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inventario</title>
+</head>
+<body>
+    <?php
+         $sqlquery =mysqli_query($connection,"SELECT * FROM inventario WHERE idProducto='LAC-01'");
+        $datos=mysqli_fetch_row($sqlquery);
+        while($datos)
+        {
+            echo $datos['0'];
+            echo $datos['1'];
+            echo $datos['2'];
+            echo $datos['3'];
+        }
+    
+    ?>
+
+    Leches: <br>
+
+    
+</body>
+</html>
