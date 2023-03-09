d3534bd35796927b4572d281819e551970c963b2
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-08 23:11:00 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -10,17 +10,15 @@
     <?php
         $connection= mysqli_connect("127.0.0.1","u614989997_admindb","6TUH72toIDh5RpVGBi","u614989997_db_inventario");
         $sqlquery =mysqli_query($connection,"SELECT * FROM inventario WHERE idProducto='LAC-01'");
-        $datos=mysqli_fetch_row($sqlquery);
-        while($datos)
-        {
-        ?>
-            <p> <?php echo $datos['0']?> </p>
-    <?php
-      }
-    ?>
- 
-    Leches: <br>
+        if($sqlquery){
+            while($datos = $sqlquery ->fetch_array(MYSQLI_ASSOC)) {
+                echo $datos["nombre"];
+                echo $datos["cantidad"];
+            }
 
-    
+        }
+
+    ?>
+
 </body>
 </html>
