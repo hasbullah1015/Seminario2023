36937c301af84549b8d1a31d69d33a712dd9f6ca
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-09 19:24:36 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
 
 $("#pie_pagina").load("/public/html/footer.html");
 $("#main_data").load("/public/html/userdata.php");
-$("db_data_panel").load("/public/php/inventario.php");
+$("#db_data_panel").load("/public/php/inventario.php");
